f2c66ceccabdb6941ce5f80434e65afb4bcaeb22
MahmoudMiehob <mahmoudmiehob@gmail.com>
2022-05-10 17:40:14 +0300

    insert in database

--- Routes/user.js
+++ Routes/user.js
@@ -3,7 +3,16 @@ const User = require("../Models/User");
 const router = express.Router();
 
 
-// CRUD => create - Read - Update - Delete - 
+router.post("/", (req, res) => {
+    const user = new User({ firstname: req.body.firstname, lastname: req.body.lastname });
+console.log(req.body);
+    user
+        .save()
+        .then((data) => res.status(200).send(data))
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+});
 
 
 module.exports = router;
